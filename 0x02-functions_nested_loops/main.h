e0f915340e5e872fd4aef92a17fd00518738a133
maraxhi <amarachiperpetua8@gmail.com>
2022-09-13 19:02:43 -0700

    hello

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "main.h"
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,5 +15,5 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /*MAIN_H*/
+#endif
 
